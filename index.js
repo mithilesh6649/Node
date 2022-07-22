85291a623e27767ff77325302c37303e44757c53
mithilesh <mithileshkumar6649@gmail.com>
2022-07-23 01:14:04 +0530

    node start basic

--- index.js
+++ index.js
@@ -12,3 +12,49 @@
 // console.log(server);
 
 // server.listen(4040);
+
+//Second 
+
+// const http   = require('http');
+ 
+// const server  =  http.createServer((request,response)=>{
+// 	response.write('Hello');
+// 	response.end();
+// });
+
+//  server.listen(8080);
+
+
+
+const http  = require('http');
+
+const server = http.createServer((request,response)=>{
+   
+   //operation 
+   	const date = new Date(); 
+    const current_date = date.toLocaleDateString(); 
+
+  // header info
+ 
+   response.writeHead(200,{
+   	 'Content-Type':'application/json'
+   });
+
+   //response in json format [so created json format in below]
+ 
+    const result = {
+    	current_date : current_date
+    };
+
+    const string_data = JSON.stringify(result);  // JSON.stringify() this function conver json object into string
+
+ // NOTE :- response.write() write mai kabhi bhi json ke format mai data pass nahi hota hai always strign pass hota hai
+   response.write(string_data);
+
+
+  //disconnect user
+   response.end(); 
+
+}); 
+
+server.listen(8080);
