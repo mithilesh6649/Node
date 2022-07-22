e2bfff5c3912055484ae52d1b05483a894e1bdb4
mithilesh <mithileshkumar6649@gmail.com>
2022-07-23 00:17:52 +0530

    node start

---
+++ index.js
@@ -0,0 +1,14 @@
+//console.log(' Welcome Node ! By Wap ');
+
+// const http = require("http"); 
+
+// const server  = http.createServer(function(request,response){
+// 	response.write("Hello Node");
+// 	console.log('Hello Node');
+// 	response.end();
+// });
+// console.log(http);
+// console.log("testing");
+// console.log(server);
+
+// server.listen(4040);
